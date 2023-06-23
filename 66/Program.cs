676d5e89f080de29ca3a47f35399323c80400354
Olga <olga252318@mail.ru>
2023-06-23 10:21:09 +0300

    66

---
+++ 66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число N: ");
+int n = int.Parse(ReadLine());
+WriteLine("Введите число M: ");
+int m = int.Parse(ReadLine());
+
+WriteLine(Summa(n,m));
+
+int Summa(int a, int b)
+{
+    if (a == b) return a;
+    else return Summa(a + 1, b) + a;
+}
+
